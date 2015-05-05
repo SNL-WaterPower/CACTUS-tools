52a59b2e4926eb914c6e45599028f2df60d246f3
Phil Chiu <whophilchiu@gmail.com>
2015-05-05 01:19:39 -0700

    Implemented argparse and --delete option
    Implemented the argparse module and added the optional --delete flag,
    which specifies that the original CSV files should be deleted after
    being converted to VTK.

--- pyCactus/scripts/pyCactusCsvToVtk.py
+++ pyCactus/scripts/pyCactusCsvToVtk.py
@@ -2,34 +2,47 @@
 # pyCactusCsvToVtk.py
 # =====================
 # Converts CACTUS instantaneous wake node and wake grid data files from CSV format to VTK using
-#   the pyCactus and pyCactusWake modules.
+# the pyCactus and pyCactusWake modules.
 #
 # The wake node data is moved to '/WakeNodeVTK' and the grid data in '/WakeGridVTK' in the 
 # Expects that the wake node data and wake grid data match the patterns:
 #
-#   [case_name]_Wake_*.csv
-#   [case_name]_WakeDef_*.csv
-# 
-#   Usage: pyCactusCsvToVtk.py [case_dir] [case_name] [output_dir]
+#    [case_name]_Wake_*.csv
+#    [case_name]_WakeDef_*.csv"""
+#
+# Usage: pyCactusCsvToVtk.py [case_path] [case_name] [output_path]
 
 import os
-import sys
 import time
 import glob
+import argparse
 
 import pyCactus
 import pyCactusWake
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print 'Not enough command line arguments.'
-        print '    Usage:    pyCactusCsvToVtk.py [case_path] [case_name] [output_dir]'
-        sys.exit()
+
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description="""Convert CACTUS wake node and grid data files from CSV to VTK using pyCactus and pyCactusWake modules.
+
+The wake node data is moved to '/WakeNodeVTK' and the grid data in '/WakeGridVTK' in the 
+Expects that the wake node data and wake grid data match the patterns:
+
+   [case_name]_Wake_*.csv
+   [case_name]_WakeDef_*.csv""")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("case_path", help="path to CSV files to be converted.", type=str)
+    parser.add_argument("case_name", help="prefix of output files, e.g. [case_name]_WakeData_1.csv", type=str)
+    parser.add_argument("output_path", help="desired output path for VTK files.", type=str)
+    parser.add_argument("--delete", help="delete original CSV files after converting.", action="store_true", default=False)
+    
+    args = parser.parse_args()
 
     # read in command-line arguments
-    case_path   = sys.argv[1]
-    case_name   = sys.argv[2]
-    output_path = sys.argv[3]
+    case_path   = args.case_path
+    case_name   = args.case_name
+    output_path = args.output_path
 
     # guess the geom filename
     geom_fname, = glob.glob('*.geom')
@@ -48,7 +61,6 @@ if __name__ == '__main__':
         except:
             print 'There was an error creating the directory' + wake_grid_out_path
 
-
     if not os.path.exists(wake_node_out_path):
         try:
             os.makedirs(wake_node_out_path)
@@ -56,12 +68,25 @@ if __name__ == '__main__':
             print 'There was an error creating the directory' + wake_node_out_path
 
     # write VTK series
+    print args.delete
     try:
         run.wakegrid.write_vtk_series(wake_grid_out_path, name=case_name + 'WakeGrid')
     except:
         print 'Error: There was an error writing the wake grid data out (possibly because it doesn\'t exist.)'
-        
+    else:
+        # delete the CSV files if the --delete flag is specified
+        if args.delete:
+            for f in run.wakegrid_filenames:
+                os.remove(f)
+                print f + ' was deleted.'
+
     try:            
         run.wakeelems.write_vtk_series(wake_node_out_path, name=case_name + 'WakeNode')
     except:
         print 'Error: There was an error writing the wake node data out (possibly because it doesn\'t exist.)'
+    else:
+        if args.delete:
+        # delete the CSV files if the --delete flag is specified
+            for f in run.wake_filenames:
+                os.remove(f)
+                print f + ' was deleted.'
