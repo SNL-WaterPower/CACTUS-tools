936825edf9f8f7bbfb62e6f1d1bdf562b009b81e
Phillip Chiu <whophilchiu@gmail.com>
2015-11-04 17:19:07 -0700

    Added recursive_glob to package dependencies

--- pyCactus/setup.py
+++ pyCactus/setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'Phillip Chiu',
     author_email = 'pchiu@sandia.gov',
     url = 'https://github.com/SNL-WaterPower/CACTUS-tools',
-    py_modules = ['pyCactus', 'pyCactusGeom', 'pyCactusWake', 'pyCactusProbes'],
+    py_modules = ['pyCactus', 'pyCactusGeom', 'pyCactusWake', 'pyCactusProbes', 'recursive_glob'],
     install_requires = ['PyEVTK', 'f90nml'],
     dependency_links=['https://bitbucket.org/pauloh/pyevtk', 'https://pypi.python.org/pypi/f90nml']
 )
