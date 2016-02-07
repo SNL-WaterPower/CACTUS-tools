19befeb5163d9dabb5913cfad5c27ebcd9f33f17
Pete Bachant <petebachant@gmail.com>
2016-02-07 11:28:26 -0500

    Clean up whitespace in CreateTurbine.m

--- geometry/CreateGeom/CreateTurbine.m
+++ geometry/CreateGeom/CreateTurbine.m
@@ -2,7 +2,7 @@ function T=CreateTurbine(NBlade,NBElem,NStrut,NSElem,RefR,RotN,RotP,RefAR,Type,v
 global rB bTwist HubRR Tilt eta bCone bi;
 % Creates a CACTUS turbine geometry structure.
 %
-% NBlade: Number of blades. 
+% NBlade: Number of blades.
 % NBElem: Number of elements per blade.
 % NStrut: Number of struts.
 % NSElem: Number of elements per strut.
@@ -10,16 +10,16 @@ global rB bTwist HubRR Tilt eta bCone bi;
 %   Will be used to normalize other distances input directly to CACTUS, and
 %   also used when outputing dimensional results from CACTUS.
 % RotN: Normal vector (size 1 x 3) of turbine rotation axis. Input value
-%   used as default when Type is empty, but will be overwritten if a Type 
+%   used as default when Type is empty, but will be overwritten if a Type
 %   is selected...
 % RotP: Origin point (size 1 x 3) on turbine rotation axis. Input value
-%   used as default when Type is empty, but will be overwritten if a Type 
+%   used as default when Type is empty, but will be overwritten if a Type
 %   is selected...
 % RefAR: Reference frontal area scaled by RefR^2. Used for
 %   force/torque/power coefficient normalization in CACTUS. Input value
-%   used as default when Type is empty, but will be overwritten if a Type 
+%   used as default when Type is empty, but will be overwritten if a Type
 %   is selected...
-% Type: Recognized generic turbine type string (see code below). Input empty to 
+% Type: Recognized generic turbine type string (see code below). Input empty to
 %   just create an empty turbine structure. If input string is
 %   recognized, will fill arrays with actual data for given turbine type
 %   using additional arguments defined below.
@@ -40,19 +40,19 @@ T.Type=Type;
 
 % Create blades
 for i=1:NBlade
-   T.B(i)=CreateBlade(NBElem); 
+   T.B(i)=CreateBlade(NBElem);
 end
 
 % Create struts
 for i=1:NStrut
-   T.S(i)=CreateStrut(NSElem); 
+   T.S(i)=CreateStrut(NSElem);
 end
 
 % Fill geometry if turbine type recognized
 if strcmp(Type,'VAWT')==1
-    
+
 	% Cross-flow turbine generator for a vertical axis wind turbine (VAWT) with either straight or parabolic blades.
-    % For NStrut > 0, struts will be evenly distributed amongst blades (NStrut must be a multiple of NBlade) and 
+    % For NStrut > 0, struts will be evenly distributed amongst blades (NStrut must be a multiple of NBlade) and
     % along rotation axis from the center to the tips.
 	% Additional arguments:
 	% REqR: Equitorial radius to reference radius ratio
@@ -66,7 +66,7 @@ if strcmp(Type,'VAWT')==1
     % Get vars
     if length(varargin)<7
         error('Not enough inputs for selected turbine type');
-    end 
+    end
     REqR=varargin{1};
     CR=varargin{2};
     HR=varargin{3};
@@ -74,15 +74,15 @@ if strcmp(Type,'VAWT')==1
     BShape=varargin{5};
     CRs=varargin{6};
     TCs=varargin{7};
-    
+
     % Ref to reference radius
     CR=CR*REqR;
     HR=HR*REqR;
-    
+
     % Set rotation axis along y
     T.RotN=[0,1,0];
     T.RotP=[0,0,0];
-    
+
     % Radius ratio function
     yB=linspace(0,HR,NBElem+1);
     if BShape
@@ -90,7 +90,7 @@ if strcmp(Type,'VAWT')==1
         rr=REqR*(1-4*(yB/HR-.5).^2);
         % Frontal area normalized by RefR^2
         T.RefAR=2*(REqR*HR-1/3*HR);
-        
+
         % Fill element end geometry
         deltac=(eta-.25)*CR;
         T.B(1).CtoR=CR*ones(1,NBElem+1);
@@ -105,7 +105,7 @@ if strcmp(Type,'VAWT')==1
         rr=REqR*ones(size(yB));
         % Frontal area normalized by RefR^2
         T.RefAR=2*REqR*HR;
-        
+
         % Fill element end geometry
         deltac=(eta-.25)*CR;
         T.B(1).CtoR=CR*ones(1,NBElem+1);
@@ -116,16 +116,16 @@ if strcmp(Type,'VAWT')==1
         T.B(1).QCy=yB;
         T.B(1).QCz=-rr;
     end
-    
+
     % Calc element geom for first blade
     T.B(1)=CalcBEGeom(T.B(1));
-    
+
     % Copy and rotate for other blades
     Phase=linspace(0,2*pi,NBlade+1);
     for i=2:NBlade
         T.B(i)=RotateBlade(T.B(1),Phase(i),T.RotN,T.RotP);
     end
-    
+
     % Fill struts on first blade
     if mod(NStrut,NBlade)~=0
         error('Number of struts must be a multiple of the number of blades for the ''VAWT'' input type.');
@@ -135,7 +135,7 @@ if strcmp(Type,'VAWT')==1
     yS=yS(2:end-1);
     rrS=interp1(yB,rr,yS);
     yC=(yB(2:end)+yB(1:end-1))/2;
-    
+
     for i=1:NSpB
         % Fill element end geometry
         T.S(i).MCx=zeros(1,NSElem+1);
@@ -147,11 +147,11 @@ if strcmp(Type,'VAWT')==1
         T.S(i).EIndS=0;
         T.S(i).BIndE=1;
         [m,T.S(i).EIndE]=min(abs(yC-yS(i)));
-        
+
         % Calc element geom
         T.S(i)=CalcSEGeom(T.S(i));
     end
-    
+
     % Copy and rotate for other blades
     for i=2:NBlade
         for j=1:NSpB
@@ -161,9 +161,9 @@ if strcmp(Type,'VAWT')==1
         end
     end
 
-    
+
 elseif strcmp(Type,'HAWT')==1
-    
+
     % Axial-flow turbine generator for a horizontal axis wind turbine (HAWT).
     % Additional arguments:
     % 1 RMaxR: Turbine radius to reference radius ratio
@@ -180,7 +180,7 @@ elseif strcmp(Type,'HAWT')==1
     % Get vars
     if length(varargin)<10
         error('Not enough inputs for selected turbine type');
-    end 
+    end
     RMaxR=varargin{1};
     HubRR=varargin{2};
     rB=varargin{3};
@@ -191,17 +191,17 @@ elseif strcmp(Type,'HAWT')==1
     eta=varargin{8};
     bCone=varargin{9};
     Tilt=varargin{10};
-    
+
     % Ref to reference radius
     CR=CR*RMaxR;
     HubRR=HubRR*RMaxR;
-    
+
     % Set rotation axis along x
     T.RotN=[1,0,0];
     T.RotP=[0,0,0];
-    
+
     % Radius ratio function
-    
+
     % Frontal area normalized by RefR^2
     T.RefAR=pi*RMaxR^2;
 
@@ -220,18 +220,18 @@ elseif strcmp(Type,'HAWT')==1
 
     % Calc element geom for first blade
     T.B(1)=CalcBEGeom(T.B(1));
-    
+
     % Rotate through incidence and coning angle
     T.B(1)=RotateBlade(T.B(1),bi/180*pi,[0,-1,0],[0,0,0]);
     T.B(1)=RotateBlade(T.B(1),bCone/180*pi,[0,0,1],[0,0,0]);
-    
+
     % Copy and rotate for other blades
     Phase=linspace(0,2*pi,NBlade+1);
     for i=2:NBlade
         T.B(i)=RotateBlade(T.B(1),Phase(i),T.RotN,T.RotP);
     end
-    
+
     % Rotate turbine through tilt angle
     T=RotateTurbine(T,Tilt/180*pi,[0,0,-1],[0,0,0]);
-    
+
 end
