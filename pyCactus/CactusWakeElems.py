5badfb024bfcce9fab421a308107e6e3e5ea8994
Phil Chiu <whophilchiu@gmail.com>
2016-04-09 20:10:58 -0700

    Add module level functions to CactusWakeElems
    Module level functions were accidentally removed from CactusWakeElems
    when the CactusField class was split off. These should eventually be
    moved to a separate module, along with other shared functions.

--- pyCactus/CactusWakeElems.py
+++ pyCactus/CactusWakeElems.py
@@ -258,3 +258,29 @@ class CactusWakeElems():
 		pvd_filename = os.path.abspath(path + '/' + collection_fname)
 		tree.write(pvd_filename, xml_declaration=True)
 		print 'Wrote ParaView collection file: ' + pvd_filename
+
+#####################################
+######### Module  Functions #########
+#####################################
+def load_data(data_filename):
+	""" load_data(data_filename) : Reads a CSV file using pandas and returns a pandas dataframe """
+	
+	reader = pd.read_csv(data_filename, iterator=True, chunksize=1000)
+	df = pd.concat(reader, ignore_index=True)
+	
+	df.rename(columns=lambda x: x.strip(), inplace=True)	# strip whitespace from colnames
+	return df
+
+
+def get_file_time(data_filename, time_col_name):
+	""" get_file_time(data_filename) : Returns the time of an instantaneous data set by reading the 
+		first two rows."""
+
+	with open(data_filename) as f:
+		header = f.readline()
+		row1   = f.readline()
+
+		time_col_num = header.split(',').index(time_col_name)
+		time = float(row1.split(',')[time_col_num])
+	
+	return time
