3d4dd313de4cc3fc60182ac10c6cc751e3c5ea07
Phil Chiu <whophilchiu@gmail.com>
2016-04-14 15:30:08 -0700

    Update docstring in CactusInput

--- pyCactus/CactusInput.py
+++ pyCactus/CactusInput.py
@@ -3,9 +3,16 @@ import f90nml
 import warnings
 
 class CactusInput(object):
-	"""Class for reading and writing CACTUS input namelist files."""
-	def __init__(self, filename):
+	"""Class for reading and writing CACTUS input namelist files.
 
+	Attributes
+	----------
+	namelist : dict
+		A dictionary with the namelist contents (from f90nml).
+	"""
+
+	def __init__(self, filename):
+		"""Reads in the CACTUS input namelist."""
 		if os.path.exists(filename):
 			# read in the input file namelist
 			self.namelist = f90nml.read(filename)
