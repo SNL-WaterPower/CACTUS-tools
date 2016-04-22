SNL-WaterPower/CACTUS-tools

commit 7fff4ce09bc07c1ee33c797a01d0bab8d71f9dc2

Fix CactusField init
Fix a bug in CactusField.__init__ which caused the initialization to
fail if self.filenames was an empty list.

pyCactus/CactusField.py
@@ -7,7 +7,6 @@ import pandas as pd
 
 from warnings import *
 
-
 class CactusField():
     """Class for reading WakeData (element) from CSV files.
 
@@ -49,7 +48,7 @@ class CactusField():
         # file from glob)
         if read_grid_dims:
             tic = pytime.time()
-            self.grid_dims = self.read_grid_dims(self.filenames[0])
+            self.grid_dims = self.read_grid_dims(self.filenames[:-1])
             print 'Read grid dimensions in %2.2f s' % (pytime.time() - tic)
 
     def read_file_headers(self):

commit 238fa140fc00fbf41bbf873eea9a7a935221faea

Fix bug in CactusField init
Fix a bug in CactusField init which was introduced in 7fff4ce which
was causing multiple filenames to be passed to read_grid_dims(), when it
should only take one. The fix puts the statement in a for loop which
breaks after first execution. This approach is safe even when
self.filenames is an empty list.

pyCactus/CactusField.py
@@ -47,9 +47,11 @@ class CactusField():
         # (note that this isn't necessarily the FIRST timestep, just the first
         # file from glob)
         if read_grid_dims:
-            tic = pytime.time()
-            self.grid_dims = self.read_grid_dims(self.filenames[:-1])
-            print 'Read grid dimensions in %2.2f s' % (pytime.time() - tic)
+            for filename in self.filenames:
+                tic = pytime.time()
+                self.grid_dims = self.read_grid_dims(filename)
+                print 'Read grid dimensions in %2.2f s' % (pytime.time() - tic)
+                break
 
     def read_file_headers(self):
         """Read the file headers, store times to instance variable.